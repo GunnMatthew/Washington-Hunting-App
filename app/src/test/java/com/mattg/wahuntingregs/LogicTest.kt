7d618873b14bfa37661d21623eac57ff1fac242c
GunnMatthew <147090365+GunnMatthew@users.noreply.github.com>
2026-06-19 01:46:44 -0700

    Pushed automated testing (hopefully?)

---
+++ app/src/test/java/com/mattg/wahuntingregs/LogicTest.kt
@@ -0,0 +1,33 @@
+package com.mattg.wahuntingregs
+
+import com.mattg.wahuntingregs.utils.extractGmuNumber
+import com.mattg.wahuntingregs.utils.isHarvestRestriction
+import org.junit.Assert.*
+import org.junit.Test
+
+class LogicTest {
+
+    // Verify correct GMU # is extracted from string
+    @Test
+    fun extractGmuNumber() {
+        val result = extractGmuNumber("101 - Sherman")
+
+        assertEquals("101", result)
+    }
+
+    // Verify harvest restrictions "Any buck"
+    @Test
+    fun isHarvestRestrictionAnyBuck() {
+        val result = isHarvestRestriction("Any Buck")
+
+        assertTrue(result)
+    }
+
+    // Verify general restrictions "Closed in GMU"
+    @Test
+    fun isHarvestRestriction_returns_false_for_general_gmu_note() {
+        val result = isHarvestRestriction("Closed in certain GMUs")
+
+        assertFalse(result)
+    }
+}
